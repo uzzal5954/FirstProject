304ef4d446a6a687f121ccb698396fdabe6bc969
Uzzal Chandra Boissha <46223743+uzzal5954@users.noreply.github.com>
2019-02-15 20:48:54 +0600

    Add files via upload

---
+++ first gitub.py
@@ -0,0 +1,5 @@
+#The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+R = int(input("enter the value :"))
+π = 3.14159
+A = π*R*R
+print("Area =",A,"unit.")
